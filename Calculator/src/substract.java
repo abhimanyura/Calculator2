aa9f041b51bad3511fef1d72851620d7cbd37cda
abhimanyura <abhimanyu.ise@gmail.com>
2017-08-02 18:04:07 +0530

    second user commit 2

--- Calculator/src/substract.java
+++ Calculator/src/substract.java
@@ -9,4 +9,12 @@ public class substract {
 		c=a-b;
 		System.out.println("substraction Resut is"+c);
 	}
+	public void substract(double a,double b)
+	{
+		 a=10.5;
+		b=20.5;
+		double c;
+		c=b-a;
+		System.out.println("substraction Resut is"+c);
+	}
 }
